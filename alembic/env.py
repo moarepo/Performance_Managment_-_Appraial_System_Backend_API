d4bfd66c2f851649093256d934f70a4af141af54
ChromeDev1 <armani.brown@moa.gov.jm>
2026-02-02 17:01:10 -0500

    multiple table added

--- alembic/env.py
+++ alembic/env.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -5,9 +8,20 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from database_config.db_modal import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+load_dotenv()
+
+db_url = os.getenv("DATABASE_URL")
+if db_url is None:
+    raise RuntimeError("Database URl is not set in the environment.")
+
+# Override sqlalchemy.url in alembic config
+config.set_main_option("sqlalchemy.url", db_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +32,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- database_config/db_modal.py
+++ database_config/db_modal.py
@@ -1,10 +1,12 @@
 import uuid
 import enum
-
-from sqlalchemy import Enum, Column, String, DateTime, ForeignKey, Boolean, false
+from sqlalchemy import Column, String, DateTime, Enum, Boolean, text, func, ForeignKey, Numeric
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime, timezone
+
+Base = declarative_base()
+
 
 class Roles(str, enum.Enum):
     OD_DIRECTOR  = "Director Oganization Development"
@@ -12,3 +14,113 @@ class Roles(str, enum.Enum):
     OD_OFFICER = "Oganization Development Officer"
     HRM = "Human Resource Management"
     HRD = "Human Resource Development"
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    user_Id = Column( 
+        UUID(as_uuid=True), 
+        primary_key=True, 
+        default=uuid.uuid4, 
+        nullable=False, 
+        server_default=text("gen_random_uuid()")
+    )
+    username =  Column(String, unique=True, nullable=False)
+    password =  Column(String, nullable=False)
+    user_role = Column(Enum(Roles, name="user_user_role"), nullable=False)
+    fristname = Column(String, nullable=False)
+    lastname = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    is_active = Column(Boolean, nullable=False, default= True)
+    DateCreated = Column(DateTime(timezone=True), default=func.now())
+
+class Divisions(Base):
+    __tablename__ = "divisions"
+
+    division_id = Column( 
+        UUID(as_uuid=True), 
+        index= True,
+        primary_key=True, 
+        default=uuid.uuid4, 
+        nullable=False, 
+        server_default=text("gen_random_uuid()")
+    )
+    division_name = Column(String, nullable=False, unique=True)
+    DateCreated = Column(DateTime(timezone=True), default=func.now())
+
+class JobTitles(Base):
+    __tablename__="job_titles_table"
+
+    job_title_id =  Column( 
+        UUID(as_uuid=True), 
+        index= True,
+        primary_key=True, 
+        default=uuid.uuid4, 
+        nullable=False, 
+        server_default=text("gen_random_uuid()")
+    )
+    job_title_name = Column(String,nullable=False,unique=True)
+    DateCreated = Column(DateTime(timezone=True), default=func.now())
+
+class PostGrades(Base):
+    __tablename__="post_grades_table"
+
+    post_grade_id = Column(
+        UUID(as_uuid=True), 
+        index= True,
+        primary_key=True, 
+        default=uuid.uuid4, 
+        nullable=False, 
+        server_default=text("gen_random_uuid()")
+    )
+    post_grade_name = Column(String,unique=True,nullable=False)
+    DateCreated = Column(DateTime(timezone=True), default=func.now())
+
+class Employess(Base):
+    __tablename__="employees"
+
+    employee_Id = Column(
+        UUID(as_uuid=True), 
+        index= True,
+        primary_key=True, 
+        default=uuid.uuid4, 
+        nullable=False, 
+        server_default=text("gen_random_uuid()")
+    )
+    employee_number = Column(String,nullable=False,unique=True)
+    fristname = Column(String,nullable=False)
+    middlename = Column(String,nullable=True)
+    lastname = Column(String,nullable=False)
+    post_number = Column(String,nullable=True)
+    job_title_id = Column(UUID(as_uuid=True), ForeignKey("job_titles_table.job_title_id"), nullable=False)
+    post_title_id = Column(UUID(as_uuid=True),ForeignKey("job_titles_table.job_title_id"), nullable=False)
+    post_grade_Id = Column(UUID(as_uuid=True), ForeignKey("post_grades_table.post_grade_id"), nullable=False)
+    divivision_id = Column(UUID(as_uuid=True), ForeignKey("divisions.division_id"), nullable=False)
+    is_active = Column(Boolean,default=True,nullable=False)
+    DateCreated = Column(DateTime(timezone=True), default=func.now())
+
+class PERs_table(Base):
+    __tablename__="pers_table"
+
+    per_id = Column(
+        UUID(as_uuid=True),
+        index=True,
+        primary_key=True,
+        default=uuid.uuid4,
+        nullable=False,
+        server_default=text("gen_random_uuid()")
+    )
+    employee_id = Column(UUID(as_uuid=True),ForeignKey("employees.employee_Id"),nullable=False)
+    period_of_evaluation = Column(String,nullable=False)
+    performance_scores = Column(Numeric(precision=5, scale=2), nullable=True)
+    pmas_scores = Column(Numeric(precision=5, scale=2), nullable=True)
+    training_needs = Column(String, nullable=True)
+    CareerAspirations = Column(String, nullable=True)
+    RatingOfficerComments = Column(String, nullable=True)
+    DocumentShearePointLink = Column(String,nullable=False)
+    DateOfSubmission = Column(DateTime(timezone=True), nullable=False)
+    ProcessingSatus = Column(UUID(as_uuid=True), nullable=False)
+    AssignedTo = Column(UUID(as_uuid=True),ForeignKey("users.user_Id"),nullable=False)
+    DateCreated = Column(DateTime(timezone=True), default=func.now())
+    UpdatedAt = Column(DateTime,nullable=True)
